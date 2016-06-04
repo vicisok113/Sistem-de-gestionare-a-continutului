e8c89545654b2f6df127619eac8923c17d1e4304
vicisok113 <tudosa.victor@yahoo.com>
2016-06-04 10:18:14 +0300

    Add files via upload

---
+++ config/fckeditor.php
@@ -0,0 +1,16 @@
+<?php
+if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config = array(
+'instanceName' => '',  
+//BasePath is the location where the system will look for the fckeditor folder.
+ 'BasePath' => base_url().'application/third_party/ckeditor/', 
+ 'ToolbarSet' => 'Default',
+ 'Width' => '100%',
+ 'Height' => '300',
+ 'Value' => ''
+              ); 
+/* End of file fckeditor.php */
+/* Location: ./application/config/fckeditor.php */
+
+?>
